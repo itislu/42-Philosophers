396e3c2bfdee6ea68a0dd18e3b20a83fc95dcda6
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-05-22 01:38:24 +0200

    improve: Reduce CPU usage of custom usleep

--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 19:15:26 by ldulling          #+#    #+#             */
-/*   Updated: 2024/05/22 01:07:07 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/05/22 01:37:07 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 bool	usleep_while_alive(useconds_t us, t_philo *philo)
 {
-	struct timeval		start_time;
-	useconds_t			sleep_time_us;
-	unsigned long long	slept_time_us;
+	struct timeval	start_time;
+	useconds_t		sleep_time_us;
+	useconds_t		slept_time_us;
 
 	gettimeofday(&start_time, NULL);
 	if (us > 1000)
@@ -25,20 +25,27 @@ bool	usleep_while_alive(useconds_t us, t_philo *philo)
 	else
 		sleep_time_us = us;
 	slept_time_us = 0;
-	while (slept_time_us + 1000 >= us)
+	while (slept_time_us + 1000 > us)
 	{
 		if (!check_alive(philo))
 			return (false);
 		usleep(sleep_time_us);
 		slept_time_us = get_elapsed_time_us(start_time);
 	}
-	while (slept_time_us < us)
-	{
-		sleep_time_us /= 2;
-		if (!check_alive(philo))
-			return (false);
-		usleep(sleep_time_us / 10);
-		slept_time_us = get_elapsed_time_us(start_time);
-	}
+	// while (slept_time_us < us)
+	// {
+		// sleep_time_us = us - slept_time_us;
+		// sleep_time_us /= 2;
+		// if (!check_alive(philo))
+		// 	return (false);
+		// usleep(sleep_time_us);
+		// usleep(sleep_time_us / 10);
+	// 	slept_time_us = get_elapsed_time_us(start_time);
+	// }
+	if (!check_alive(philo))
+		return (false);
+	slept_time_us = get_elapsed_time_us(start_time);
+	if (slept_time_us < us)
+		usleep(us - slept_time_us);
 	return (true);
 }
