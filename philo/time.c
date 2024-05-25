3622f274d0e0de2772057bd145fb5ad7be53990d
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-05-25 14:53:39 +0200

    fix: Fix wrong time conversions
    Sometimes 1 millisecond was missing.

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 19:37:28 by ldulling          #+#    #+#             */
-/*   Updated: 2024/05/20 18:38:38 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/05/25 14:23:51 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 unsigned long long	get_elapsed_time_ms(struct timeval start_time)
 {
 	struct timeval		current_time;
+	struct timeval		result;
 
 	gettimeofday(&current_time, NULL);
-	return ((current_time.tv_sec - start_time.tv_sec) * 1000 +
-			(current_time.tv_usec - start_time.tv_usec) / 1000);
+	timersub(&current_time, &start_time, &result);
+	return (result.tv_sec * 1000 + result.tv_usec / 1000);
 }
 
 unsigned long long	get_elapsed_time_us(struct timeval start_time)
 {
 	struct timeval		current_time;
+	struct timeval		result;
 
 	gettimeofday(&current_time, NULL);
-	return ((current_time.tv_sec - start_time.tv_sec) * 1000000 +
-			(current_time.tv_usec - start_time.tv_usec));
+	timersub(&current_time, &start_time, &result);
+	return (result.tv_sec * 1000000 + result.tv_usec);
 }
