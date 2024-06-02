01305c03a3ddaea31073bb0328a6f090d2754fee
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-06-02 14:45:29 +0200

    refactor: Split up single monitoring cycle and endless monitor

--- philo/monitor.c
+++ philo/monitor.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 16:55:06 by ldulling          #+#    #+#             */
-/*   Updated: 2024/06/02 13:53:35 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/06/02 14:42:51 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,38 +53,40 @@ void	broadcast_death(t_philo *philos, int number_of_philosophers)
 	}
 }
 
-void	monitor(t_philo *philos, t_rules rules)
+t_state	monitor_cycle(t_philo *philos, int number_of_philosophers)
 {
-	int		number_of_philosophers;
 	int		i;
 	t_state	total_state;
 	t_state	philo_state;
 
-	number_of_philosophers = rules.number_of_philosophers;
 	total_state = ALIVE;
+	i = 0;
+	while (i < number_of_philosophers)
+	{
+		pthread_mutex_lock(philos[i].state_mutex);
+		philo_state = philos[i].state;
+		pthread_mutex_unlock(philos[i].state_mutex);
+		total_state = (total_state | philo_state) & (~FULL | philo_state);
+		if (total_state & DEAD)
+		{
+			broadcast_death(philos, number_of_philosophers);
+			print_death(philos, number_of_philosophers, i);
+			return (total_state);
+		}
+		i++;
+	}
+	return (total_state);
+}
+
+void	monitor(t_philo *philos, t_rules rules)
+{
+	t_state	state;
+
 	while (true)
 	{
-		i = 0;
-		while (i < number_of_philosophers)
-		{
-			pthread_mutex_lock(philos[i].state_mutex);
-			philo_state = philos[i].state;
-			pthread_mutex_unlock(philos[i].state_mutex);
-			if (philo_state & DEAD)
-			{
-				broadcast_death(philos, number_of_philosophers);
-				print_death(philos, number_of_philosophers, i);
-				return ;
-			}
-			total_state = (total_state | philo_state) & (~FULL | philo_state);
-			i++;
-		}
-		if (total_state & FULL)
-		{
-			if (VERBOSE)
-				dprintf(2, "One philosopher has eaten enough meals.\n");
-			break ;
-		}
+		state = monitor_cycle(philos, rules.number_of_philosophers);
+		if (state & (DEAD | FULL))
+			return ;
 		usleep(MONITOR_INTERVAL_US);
 	}
 }
