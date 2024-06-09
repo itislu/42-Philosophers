e226e1e5b62762bdee9bdaf6cd2b41cc54116265
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-06-10 00:39:30 +0200

    fix: Fix test case `3 1000 200 700 5` caused by negative overflow in `calc_think_us()`

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 14:54:03 by ldulling          #+#    #+#             */
-/*   Updated: 2024/06/02 16:01:05 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/06/10 00:31:16 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,16 +69,22 @@ static bool	philo_sleep(t_philo *me)
 
 static useconds_t	calc_think_us(t_philo *me, useconds_t time_to_think_us)
 {
+	int	time_to_think_ms;
+
 	if (me->rules->number_of_philosophers % 2 == 1)
 	{
 		if ((me->meals_eaten - 1) % (me->rules->number_of_philosophers / 2) == (me->id - 1 - (me->id == me->rules->number_of_philosophers)) / 2ULL)
 		{
 			me->is_outsider = true;
-			return ((me->rules->time_to_eat_ms * 2 - me->rules->time_to_sleep_ms - MARGIN_MS / 2) * 1000U);
+			time_to_think_ms = me->rules->time_to_eat_ms * 2 - me->rules->time_to_sleep_ms;
 		}
-		me->is_outsider = false;
-		if (me->rules->time_to_eat_ms - me->rules->time_to_sleep_ms > MARGIN_MS / 2)
-			return ((me->rules->time_to_eat_ms - me->rules->time_to_sleep_ms - MARGIN_MS / 2) * 1000U);
+		else
+		{
+			me->is_outsider = false;
+			time_to_think_ms = me->rules->time_to_eat_ms - me->rules->time_to_sleep_ms;
+		}
+		if (time_to_think_ms > MARGIN_MS / 2)
+			return ((time_to_think_ms - MARGIN_MS / 2) * 1000U);
 		else
 			return (0);
 	}
