a413fd51a17d4e83d5ae905afe7c7277a11e912b
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-06-10 06:23:47 +0200

    style: Norm time folder

--- philo/source/time/time.c
+++ philo/source/time/time.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 19:37:28 by ldulling          #+#    #+#             */
-/*   Updated: 2024/05/26 16:29:41 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/06/10 06:16:12 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 unsigned long long	get_elapsed_time_ms(struct timeval *start_time)
 {
-	struct timeval		current_time;
-	struct timeval		result;
+	struct timeval	current_time;
+	struct timeval	result;
 
 	gettimeofday(&current_time, NULL);
 	timersub(&current_time, start_time, &result);
@@ -24,8 +24,8 @@ unsigned long long	get_elapsed_time_ms(struct timeval *start_time)
 
 unsigned long long	get_elapsed_time_us(struct timeval *start_time)
 {
-	struct timeval		current_time;
-	struct timeval		result;
+	struct timeval	current_time;
+	struct timeval	result;
 
 	gettimeofday(&current_time, NULL);
 	timersub(&current_time, start_time, &result);
