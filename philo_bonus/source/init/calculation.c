de82146d6fb442e6c7d72aaf7c76553202fbc304
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-08-03 19:12:50 +0200

    fix: Fix under- and overflow potential in think time calculations
    Also combine the initial think time calculation for odd and even number of philos. Other than the outsider, they should always be the same, and any optimizations can be applied to both scenarios.

--- philo_bonus/source/init/calculation.c
+++ philo_bonus/source/init/calculation.c
@@ -6,14 +6,11 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 05:01:01 by ldulling          #+#    #+#             */
-/*   Updated: 2024/07/31 18:34:05 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/08/03 19:00:35 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "init_priv.h"
-
-static unsigned long long	calc_initial_even_us(int id);
-static unsigned long long	calc_initial_odd_us(const t_rules *rules, int id);
 
 bool	calc_is_outsider(const t_rules *rules, int id)
 {
@@ -25,7 +22,8 @@ bool	calc_is_outsider(const t_rules *rules, int id)
 
 unsigned long long	calc_think_time_us(const t_rules *rules)
 {
-	if (rules->time_to_eat_ms > rules->time_to_sleep_ms + MARGIN_MS / 2)
+	if ((unsigned long long)rules->time_to_eat_ms
+		> (unsigned long long)rules->time_to_sleep_ms + MARGIN_MS / 2)
 		return ((rules->time_to_eat_ms - rules->time_to_sleep_ms
 				- MARGIN_MS / 2) * 1000ULL);
 	else
@@ -36,24 +34,13 @@ unsigned long long	calc_initial_think_time_us(const t_rules *rules, int id)
 {
 	if (rules->num_of_philos < 2)
 		return ((rules->time_to_die_ms + 1ULL) * 1000ULL);
-	else if (rules->num_of_philos % 2 == 0)
-		return (calc_initial_even_us(id));
-	else
-		return (calc_initial_odd_us(rules, id));
-}
-
-static unsigned long long	calc_initial_even_us(int id)
-{
-	if (id % 2 == 0)
-		return (MARGIN_MS / 2 * 1000);
-	else
-		return (0);
-}
-
-static unsigned long long	calc_initial_odd_us(const t_rules *rules, int id)
-{
-	if (calc_is_outsider(rules, id))
-		return ((rules->time_to_eat_ms * 2ULL - MARGIN_MS / 2) * 1000ULL);
+	else if (calc_is_outsider(rules, id))
+	{
+		if (rules->time_to_eat_ms * 2ULL > MARGIN_MS)
+			return ((rules->time_to_eat_ms * 2ULL - MARGIN_MS / 2) * 1000ULL);
+		else
+			return (MARGIN_MS);
+	}
 	else if (id % 2 == 0)
 		return (MARGIN_MS / 2 * 1000);
 	else
